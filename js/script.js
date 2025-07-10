a901b40057c507d15f5e8c4e6b74adb7c533f3f2
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-10 14:41:40 +0200

    [upd] index.html (formatting, new input fields), result div for user [upd] script.js (formatting, adding better comments, new variables, innerHTML injection

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 // SVOLGIMENTO ESERCIZIO
-
+// Dichiaro la variabile che fa riferimento a send di HTML
 const button = document.getElementById("send");
-
+// Aggiungo eventListener al click del button
 button.addEventListener(`click`, function (e) {
   e.preventDefault();
 
-  const passengerAge = document.getElementById(`age`).value;
-  const passengerKm = document.getElementById(`km`).value;
-
+  const passengerName = document.getElementById("name").value; // Recupero il valore di name, surname, age e km
+  const passengerSurname = document.getElementById("surname").value;
+  const passengerAge = parseInt(document.getElementById("age").value);
+  const passengerKm = parseFloat(document.getElementById("km").value);
+	// Dichiaro le variabili che stabiliscono prezzo * km, la formula per calcolare il cost, e la variabile che conterrà il prezzo finale
   let priceKm = 0.21;
   let cost = priceKm * passengerKm;
   let finalCost;
-
+	// Con if eseguo la formula per applicare lo sconto in base all'età
   if (passengerAge <= 17) {
     finalCost = cost - (cost * 20) / 100;
   } else if (passengerAge >= 65) {
@@ -20,6 +22,11 @@ button.addEventListener(`click`, function (e) {
     finalCost = cost;
   }
 
-  // UTILIZZO TOFIXED(2) PER MOSTRARE LA SECONDA CIFRA DOPO LA VIRGOLA DEL RISULTATO DECIMALE
-  console.log("Il costo del tuo viaggio è di €", finalCost.toFixed(2));
+  // INSERISCO IN HTML passengerName, passengerSurname, finalCost (fixato per il decimale)
+  document.getElementById("result").innerHTML = `
+    <p><strong>Nome:</strong> ${passengerName}</p>
+    <p><strong>Cognome:</strong> ${passengerSurname}</p>
+    <p><strong>Costo del biglietto:</strong> € ${finalCost.toFixed(2)}</p>
+  `;
 });
+
